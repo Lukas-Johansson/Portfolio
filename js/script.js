0d09ed18cc69ed36a79f8f3059b89dcba12bfc76
Lukas <lukas.johansson090@gmail.com>
2024-08-11 01:06:40 +0200

    Better animation and data

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+// script.js
 function toggleProject() {
-    var projectDetails = document.getElementById("projectDetails");
-    if (projectDetails.classList.contains("show")) {
-        projectDetails.classList.remove("show");
+    const projectDetails = document.getElementById('projectDetails');
+    const arrowIcon = document.getElementById('arrow-icon');
+
+    if (projectDetails.classList.contains('show')) {
+        projectDetails.classList.remove('show');
+        arrowIcon.src = '/images/openarrow.svg';
     } else {
-        projectDetails.classList.add("show");
+        projectDetails.classList.add('show');
+        arrowIcon.src = '/images/closearrow.svg';
     }
 }
